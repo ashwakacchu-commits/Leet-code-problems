c5ebe8eb69d9de9d3b2c155d91e390146386f251
shaik ashwak <ashwakacchu@gmail.com>
2026-07-27 11:17:25 +0530

    Create 1226. The Dining Philosophers.java

---
+++ 1226. The Dining Philosophers.java
@@ -0,0 +1,42 @@
+import java.util.concurrent.Semaphore;
+
+class DiningPhilosophers {
+    
+    private Semaphore[] forks;
+
+    public DiningPhilosophers() {
+        forks = new Semaphore[5];
+        for (int i = 0; i < 5; i++) {
+            forks[i] = new Semaphore(1);
+        }
+    }
+
+    public void wantsToEat(int philosopher,
+                           Runnable pickLeftFork,
+                           Runnable pickRightFork,
+                           Runnable eat,
+                           Runnable putLeftFork,
+                           Runnable putRightFork) throws InterruptedException {
+        
+        int leftFork = philosopher;
+        int rightFork = (philosopher + 1) % 5;
+        
+        if (philosopher % 2 == 0) {
+            forks[leftFork].acquire();
+            forks[rightFork].acquire();
+        } else {
+            forks[rightFork].acquire();
+            forks[leftFork].acquire();
+        }
+        
+        pickLeftFork.run();
+        pickRightFork.run();
+        eat.run();
+        
+        putLeftFork.run();
+        putRightFork.run();
+        
+        forks[leftFork].release();
+        forks[rightFork].release();
+    }
+}
