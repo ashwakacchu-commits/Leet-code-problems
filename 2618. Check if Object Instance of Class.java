f7dc60a0e370fa29303a0a167089d2988148915e
shaik ashwak <ashwakacchu@gmail.com>
2026-07-27 11:21:09 +0530

    Create 2618. Check if Object Instance of Class.java

---
+++ 2618. Check if Object Instance of Class.java
@@ -0,0 +1,17 @@
+
+var checkIfInstanceOf = function(obj, classFunction) {
+    if (obj === null || obj === undefined || typeof classFunction !== 'function') {
+        return false;
+    }
+    
+    let currentPrototype = Object.getPrototypeOf(obj);
+    
+    while (currentPrototype !== null) {
+        if (currentPrototype === classFunction.prototype) {
+            return true;
+        }
+        currentPrototype = Object.getPrototypeOf(currentPrototype);
+    }
+    
+    return false;
+};
